babdea42cb3cea9476a7818ad45065e25511153e
Isaac Sikkema <isaacsikkema@gmail.com>
2019-09-16 18:23:00 -0400

    mac again

--- cookie_crypt.js
+++ cookie_crypt.js
@@ -1,30 +1,6 @@
 const aes = require('aes-js');
 const pbkdf2 = require('pbkdf2');
 
-function aesDecryptor(password, salt, iterations, keylen, iv) {
-    var key = pbkdf2.pbkdf2Sync(password, salt, iterations, keylen);
-
-    return (ctext) => {
-        var cipher;
-        var buf;
-        var mtext;
-
-        cipher = new aes.ModeOfOperation.cbc(key, iv);    // Must define cipher in function because js is stupid.
-                                                                    // Probably decrypt is getting called before cipher is made or something.
-
-        ctext = ctext.slice(3);
-        buf = Buffer.from(cipher.decrypt(ctext));
-        buf = 
-            buf.slice(
-                0,
-                -1 * buf[buf.length-1]
-            );
-        mtext = buf.toString();
-        
-        return mtext;
-    };
-}
-
 class ChromeCrypt {
     constructor() {
         if (process.platform === "win32") {
@@ -36,25 +12,45 @@ class ChromeCrypt {
 
             if (process.platform === "darwin") {
                 const keychain = require('keychain');
-
-                this.iterations = 1003;
-                keychain.getPassword(
-                    {account: 'Chrome Safe Storage', service: 'Chrome'},
+                this.password = keychain.getPassword(
+                    {account: 'Chrome', service: 'Chrome Safe Storage'},
                     function(err, pass) {
                         if (err) {
                             throw err;
                         }
 
-                        console.log("!!!!!!!!", pass);
-                        this.decrypt = aesDecryptor(pass, this.salt, this.iterations, this.keylen, this.iv);
+                        return pass;
                     }
                 );
+
+                console.log(this.password);
+                this.iterations = 1003;
             } else { // process.platform === "linux", "freebsd", "sunos"
                 this.password = 'peanuts';
                 this.iterations = 1;
-                
-                this.decrypt = aesDecryptor(this.password, this.salt, this.iterations, this.keylen, this.iv);
             }
+
+            this.key = pbkdf2.pbkdf2Sync(this.password, this.salt, this.iterations, this.keylen);
+
+            this.decrypt = (ctext) => {
+                var cipher;
+                var buf;
+                var mtext;
+
+                cipher = new aes.ModeOfOperation.cbc(this.key, this.iv);    // Must define cipher in function because js is stupid.
+                                                                            // Probably decrypt is getting called before cipher is made or something.
+
+                ctext = ctext.slice(3);
+                buf = Buffer.from(cipher.decrypt(ctext));
+                buf = 
+                    buf.slice(
+                        0,
+                        -1 * buf[buf.length-1]
+                    );
+                mtext = buf.toString();
+                
+                return mtext;
+            };
         }
     }
 }
