c0eb477631a06e77fa3413bbcfc790a7ba3d67f5
Kishan Tailor <tailorkishan123@gmail.com>
2019-09-26 13:37:40 -0400

    #12 Redid Local DB with SQLlite. Need to add DB architecture

--- createDB.js
+++ createDB.js
@@ -1,24 +1,41 @@
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "yourusername",
-  password: "yourpassword"
+const sqlite3 = require('sqlite3').verbose();
+ 
+// open database in memory
+let db = new sqlite3.Database(':memory:',sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
+var sql = 'CREATE TABLE cookies (name, int)';
+var sql2 = 'INSERT INTO cookies (name) VALUES (1)'
+var sql3 = 'SELECT name Name FROM cookies'
+db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    db.all(sql2, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+    });
+    db.all(sql3, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        rows.forEach((row) => {
+          console.log(row.Name);
+        });
+    });
+    //rows.forEach((row) => {
+      //console.log(row.name);
+    //});
 });
 
-/*
-var sql2 = "CREATE TABLE cookies (name VARCHAR(255))";
-  con.query(sql2, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
   });
-*/
