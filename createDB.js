9663e85c3940ab971551515380f44a67b1ad7535
Kishan Tailor <tailorkishan123@gmail.com>
2019-09-26 15:56:27 -0400

    #12 Added architecture to the local DB

--- createDB.js
+++ createDB.js
@@ -8,9 +8,9 @@ let db = new sqlite3.Database(':memory:',sqlite3.OPEN_READWRITE, (err) => {
   console.log('Connected to the in-memory SQlite database.');
 });
 
-var sql = 'CREATE TABLE cookies (name, int)';
-var sql2 = 'INSERT INTO cookies (name) VALUES (1)'
-var sql3 = 'SELECT name Name FROM cookies'
+var sql = 'CREATE TABLE cookies(creation_utc INTEGER NOT NULL,host_key TEXT NOT NULL,name TEXT NOT NULL,value TEXT NOT NULL,path TEXT NOT NULL,expires_utc INTEGER NOT NULL,is_secure INTEGER NOT NULL,is_httponly INTEGER NOT NULL,last_access_utc INTEGER NOT NULL,has_expires INTEGER NOT NULL DEFAULT 1,is_persistent INTEGER NOT NULL DEFAULT 1,priority INTEGER NOT NULL DEFAULT 1,encrypted_value BLOB DEFAULT \'\',samesite INTEGER NOT NULL DEFAULT -1,UNIQUE (host_key, name, path))';
+var sql2 = 'INSERT INTO cookies (creation_utc, host_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc) VALUES (1, "hello", "hello", "hello", "hello", 1, 1, 1, 1)'
+var sql3 = 'SELECT host_key Name FROM cookies'
 db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
@@ -28,9 +28,6 @@ db.all(sql, [], (err, rows) => {
           console.log(row.Name);
         });
     });
-    //rows.forEach((row) => {
-      //console.log(row.name);
-    //});
 });
 
 db.close((err) => {
