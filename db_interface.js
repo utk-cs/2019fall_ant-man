c280763497ddc425d7f629273e6f09e6c38c7941
Isaac Sikkema <isaacsikkema@gmail.com>
2019-10-01 11:30:36 -0400

    Added local and caching functions #13

--- db_interface.js
+++ db_interface.js
@@ -3,6 +3,7 @@ const homedir = require('os').homedir();
 const cc = require('./cookie_crypt');
 
 var DBPATH;
+const DBLOCAL = './cookies.db';
 if (process.platform === "linux") {
     DBPATH = `${homedir}/.config/google-chrome/Default/Cookies`;
 } else if (process.platform === "darwin") {
@@ -23,6 +24,36 @@ class ChromeDB {
             var rv = stmt.get(cookie);
         } catch {
             db.close();
+        }
+
+        return rv;
+    }
+
+    static getCookieLocal(cookie) {
+        var db = new sqlite3(DBLOCAL);
+
+        try {
+            var stmt = db.prepare(
+                "SELECT * FROM cookies WHERE " +
+                    "host_key=@host_key AND " +
+                    "name=@name AND " +
+                    "path=@path"
+            );
+            var rv = stmt.get(cookie);
+        } catch {
+            db.close();
+        }
+
+        return rv;
+    }
+
+    static getCookie(cookie) {
+        var rv;
+
+        rv = this.getCookieLocal(cookie);
+        if (rv === undefined) {
+            rv = this.getCookieChrome(cookie);
+            this.addCookieLocal(rv);
         }
 
         return rv;
