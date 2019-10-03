b2ecaade96bbc06daa7806bea71ab54cc8142b26
Isaac Sikkema <isaacsikkema@gmail.com>
2019-10-03 14:17:11 -0400

    Added syncLocal to db_interface which syncs the Chrome db with the local db #13

--- db_interface.js
+++ db_interface.js
@@ -281,5 +281,123 @@ class ChromeDB {
             this.deleteCookieLocal(cookie)
         );
     }
+
+    static syncLocal() {
+        var db = new sqlite3(DBPATH);
+        var dblocal = new sqlite3(DBLOCAL);
+
+        var rv = {
+            changes: 0,
+            lastInsertRowId: -1
+        };
+        try {
+            var stmtSelect = db.prepare("SELECT * FROM cookies");
+            var stmtFind = dblocal.prepare("SELECT EXISTS(SELECT 1 FROM cookies WHERE host_key=@host_key AND name=@name AND path=@path) AS Found");
+            var stmtFindExact = dblocal.prepare(
+                "SELECT EXISTS(" +
+                    "SELECT 1 FROM cookies WHERE " +
+                    "host_key=@host_key AND " +
+                    "name=@name AND " +
+                    "path=@path AND " +
+                    "creation_utc=@creation_utc AND " +
+                    "host_key=@host_key AND " +
+                    "name=@name AND " +
+                    "value=@value AND " +
+                    "path=@path AND " +
+                    "expires_utc=@expires_utc AND " +
+                    "is_secure=@is_secure AND " +
+                    "is_httponly=@is_httponly AND " +
+                    "last_access_utc=@last_access_utc AND " +
+                    "has_expires=@has_expires AND " +
+                    "is_persistent=@is_persistent AND " +
+                    "priority=@priority AND " +
+                    "encrypted_value=@encrypted_value AND " +
+                    "samesite=@samesite" +
+                ") AS Found"
+            );
+            var stmtUpdate = dblocal.prepare(
+                "UPDATE cookies SET " +
+                    "creation_utc = @creation_utc, " +
+                    "host_key = @host_key, " +
+                    "name = @name, " +
+                    "value = @value, " +
+                    "path = @path, " +
+                    "expires_utc = @expires_utc, " +
+                    "is_secure = @is_secure, " +
+                    "is_httponly = @is_httponly, " +
+                    "last_access_utc = @last_access_utc, " +
+                    "has_expires = @has_expires, " +
+                    "is_persistent = @is_persistent, " +
+                    "priority = @priority, " +
+                    "encrypted_value = @encrypted_value, " +
+                    "samesite = @samesite " +
+                "WHERE " +
+                    "host_key = @host_key AND " +
+                    "name = @name AND " +
+                    "path = @path"
+            );
+            var stmtInsert = dblocal.prepare(
+                "INSERT INTO cookies (" +
+                    "creation_utc, " +
+                    "host_key, " +
+                    "name, " +
+                    "value, " +
+                    "path, " +
+                    "expires_utc, " +
+                    "is_secure, " +
+                    "is_httponly, " +
+                    "last_access_utc, " +
+                    "has_expires, " +
+                    "is_persistent, " +
+                    "priority, " +
+                    "encrypted_value, " +
+                    "samesite" +
+                ") VALUES (" +
+                    "@creation_utc, " +
+                    "@host_key, " +
+                    "@name, " +
+                    "@value, " +
+                    "@path, " +
+                    "@expires_utc, " +
+                    "@is_secure, " +
+                    "@is_httponly, " +
+                    "@last_access_utc, " +
+                    "@has_expires, " +
+                    "@is_persistent, " +
+                    "@priority, " +
+                    "@encrypted_value, " +
+                    "@samesite" +
+                ")"
+            );
+
+            var cookie;
+            var info;
+
+            var rows = stmtSelect.all();
+            for (cookie of rows) {
+                info = stmtFindExact.get(cookie);   // Check if already synced
+
+                if (info.Found === 0) { // Not synced
+                    info = stmtFind.get(cookie);    // Check if needs updating or inserting
+
+                    if (info.Found === 1) { // Update
+                        info = stmtUpdate.run(cookie);
+                    } else {    // Insert
+                        info = stmtInsert.run(cookie);
+                    }
+                } else {
+                    continue;
+                }
+
+                rv.changes += info.changes;
+                rv.lastInsertRowId = info.lastInsertRowid;
+            }
+        } catch(err) {
+            db.close();
+            dblocal.close();
+        }
+
+        return rv;
+    }
 }
 ChromeDB.cipher = new cc.ChromeCrypt();
