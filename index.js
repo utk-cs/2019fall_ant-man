c25f9f8e9b4daab176db42da8d740831cfd8966d
Kishan Tailor <Tailorkishan123@gmail.com>
2019-10-24 09:42:40 -0400

    #43 merge conflict resolution

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 var mode = 0;
+var globalCookie ; // cookie to store the present cookie being shown on the detailed view
+var oldCookie; //Copy of cookie before modification. 
 const DBI = require('./db_interface').ChromeDB;
 const CC = require('./cookie_crypt');
 
 console.log("this works");
 
 function updateDetailedView(cookie){
+    globalCookie = cookie;
     console.log(cookie);
-    for(var key in cookie){
+    var cipher = new CC.ChromeCrypt();
+    for (var key in cookie) {
+        if (key === "encrypted_value") {
+            continue;
+        }
+        
+        value = cookie[key];
+        if (key === "value") {
+            if (cookie["encrypted_value"] !== null) {
+                value = cipher.decrypt(cookie["encrypted_value"]);
+            }
+        } else if (key == "creation_utc") {
+            value = numToDate(cookie["creation_utc"])
+        }
+
         console.log(key);
-        console.log(cookie[key]);
-        if(cookie[key] === ''){
+        console.log(value);
+        if(value === ''){
             htmlstr = "<p>" + "''" + "</p>";
         }else{
-            htmlstr = "<p>" + cookie[key] + "</p>";
+            htmlstr = "<p>" + value + "</p>";
         }
         $("#"+key).html(htmlstr);
     }
@@ -66,8 +83,14 @@ function updateTable() {
             }
             else if (key == "creation_utc") {
                 value = numToDate(cookie["creation_utc"])
-            }  
-            // console.log("cookie time is: ", numToDate(cookie.creation_utc))
+            }
+            else if(key == "expires_utc")  {
+                value = numToDate(cookie["expires_utc"])
+            }
+            else if(key == "last_access_utc")  {
+                value = numToDate(cookie["last_access_utc"])
+            }
+           
 
             row.append(`<td>${value}</td>`);
         }
@@ -100,53 +123,56 @@ function dateToNum(date){
     return (offset*1000);
 }
 
-function modifyCookie(cookie){
-    console.log(cookie);
-    if(mode == 0){
-        var htmlstr;
-        for(var key in cookie){
-            if(key == "expires_utc"){
-                var date = numToDate(cookie[key]);
-                htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
-            }else if(key == "creation_utc"){
-                var date = numToDate(cookie[key]);
-                htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
-            }else if(key == "last_access_utc"){
-                var date = numToDate(cookie[key]);
-                htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
-            }else{
-                htmlstr = '<input type="text" id="' + key + 'input" class="form-control form-control-lg align-self-stretch" placeholder="' + cookie[key] + '" >';
-            }
-            $("#"+key).html(htmlstr);
-        }
-        document.getElementById("Modify").innerHTML = "Submit";
-        mode = 1;
-    }else{
-        var newCookie = {};
-        for(var key in cookie){
-            var val = document.getElementById(key + "input").value;
-            if(key == "expires_utc"){
-                var date = new Date(val);
-                newCookie[key] = dateToNum(date);
-            }else if(key == "creation_utc"){
-                var date = new Date(val);
-                newCookie[key] = dateToNum(date);
-            }else if(key == "last_access_utc"){
-                var date = new Date(val);
-                newCookie[key] = dateToNum(date);
-            }else{
-                if(val === ""){
-                    newCookie[key] = cookie[key];
+function modifyCookie(){
+    if(globalCookie != undefined){
+        if(mode == 0){
+            var htmlstr;
+            for(var key in globalCookie){
+                if(key == "expires_utc"){
+                    var date = numToDate(globalCookie[key]);
+                    htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
+                }else if(key == "creation_utc"){
+                    var date = numToDate(globalCookie[key]);
+                    htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
+                }else if(key == "last_access_utc"){
+                    var date = numToDate(globalCookie[key]);
+                    htmlstr = '<input type="datetime-local" id="' + key + 'input" min="1601-01-01T00:00" max="3000-01-01T00:00" class="form-control form-control-lg align-self-stretch" value="' + date.toISOString().replace('Z', '') +'">';
                 }else{
-                    newCookie[key] = val;
+                    htmlstr = '<input type="text" id="' + key + 'input" class="form-control form-control-lg align-self-stretch" placeholder="' + globalCookie[key] + '" >';
                 }
+                $("#"+key).html(htmlstr);
             }
-            
-            
+            document.getElementById("Modify").innerHTML = "Submit";
+            mode = 1;
+        }else{
+            var newCookie = {};
+            for(var key in globalCookie){
+                var val = document.getElementById(key + "input").value;
+                if(key == "expires_utc"){
+                    var date = new Date(val);
+                    newCookie[key] = dateToNum(date);
+                }else if(key == "creation_utc"){
+                    var date = new Date(val);
+                    newCookie[key] = dateToNum(date);
+                }else if(key == "last_access_utc"){
+                    var date = new Date(val);
+                    newCookie[key] = dateToNum(date);
+                }else{
+                    if(val === ""){
+                        newCookie[key] = cookie[key];
+                    }else{
+                        newCookie[key] = val;
+                    }
+                }
+                
+                
+            }
+            oldCookie = globalCookie;
+            updateDetailedView(newCookie);
+            globalCookie = newCookie;
+            document.getElementById("Modify").innerHTML = "Modify Cookie";
+            mode = 0;
         }
-        updateDetailedView(newCookie);
-        document.getElementById("Modify").innerHTML = "Modify Cookie";
-        mode = 0;
     }
 }
 
